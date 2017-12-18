5b556dd4ccec6711177614b4eabac6efbcee3606
lightningexperience <lightningexperience@users.noreply.github.com>
2017-12-18 12:00:31 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("CLIENT_ID", "3MVG98SW_UPr.JFhVeKJeObFLKswRtW9aEGxpMQ2unLDIvr_66gKuHe6zfFrBaS9pPE.NSrBRvwW62z6TxMNI");
+define("CLIENT_SECRET", "3849085924597142738");
+define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php");
+define("LOGIN_URI", "https://login.salesforce.com");
+?>
