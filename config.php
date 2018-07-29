3a1c499fe492b8b42660bd46999897d7f347e0cb
lightningexperience <lightningexperience@users.noreply.github.com>
2018-07-28 20:41:02 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define("CLIENT_ID", "3MVG9xOCXq4ID1uE0MRhgm7ZNA5zfCl2xZUq9UK0vDLUW8ZxoMZzPV0URIul.LBuZlsWYI5LXQw0z.FA9.YYy");
 define("CLIENT_SECRET", "6192512985480207832");
-define("REDIRECT_URI", "https://github.com/lightningexperience/phprest01/oauth_callback.php");
+define("REDIRECT_URI", "https://phprest001.herokuapp.com/oauth_callback.php");
 define("LOGIN_URI", "https://login.salesforce.com");
 ?>
