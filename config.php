95520dab5c0721f0742c67194de881c2f60d2454
lightningexperience <lightningexperience@users.noreply.github.com>
2018-07-28 20:37:09 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("CLIENT_ID", "3MVG98SW_UPr.JFhVeKJeObFLKswRtW9aEGxpMQ2unLDIvr_66gKuHe6zfFrBaS9pPE.NSrBRvwW62z6TxMNI");
-define("CLIENT_SECRET", "3849085924597142738");
+define("CLIENT_ID", "3MVG9xOCXq4ID1uE0MRhgm7ZNA5zfCl2xZUq9UK0vDLUW8ZxoMZzPV0URIul.LBuZlsWYI5LXQw0z.FA9.YYy");
+define("CLIENT_SECRET", "6192512985480207832");
 define("REDIRECT_URI", "https://github.com/lightningexperience/phprest01/oauth_callback.php");
 define("LOGIN_URI", "https://login.salesforce.com");
 ?>
