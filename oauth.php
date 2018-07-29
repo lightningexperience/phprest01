c8aaf144ea98687d7a3aa15e6bfb11abfdc6abe9
lightningexperience <lightningexperience@users.noreply.github.com>
2018-07-28 21:00:41 -0400

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once 'https://phprest001.herokuapp.com/config.php';
 
 $auth_url = LOGIN_URI
         . "/services/oauth2/authorize?response_type=code&client_id="
