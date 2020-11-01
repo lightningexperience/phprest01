4615891d4ee923c128458728450a0b9ad989f846
lightningexperience <lightningexperience@users.noreply.github.com>
2020-11-01 01:51:52 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("CLIENT_ID", "3MVG96hCAx1bhPagfWpX_yae7iDbUtk35unmmDaiHW3lYztWElDhS6xJum6LibHhDsjsvxZVE91cXDUC8q3qn");
-define("CLIENT_SECRET", "7AD6F6DA0B15136D4AA2A0C1BB04B7AA4EBF7C92EA3EE02E8AB17900F0EE68BA");
+define("CLIENT_ID", "3MVG9xOCXq4ID1uE0MRhgm7ZNA5zfCl2xZUq9UK0vDLUW8ZxoMZzPV0URIul.LBuZlsWYI5LXQw0z.FA9.YYy");
+define("CLIENT_SECRET", "6192512985480207832");
 define("REDIRECT_URI", "https://phprest001.herokuapp.com/oauth_callback.php");
 define("LOGIN_URI", "https://login.salesforce.com");
 ?>
